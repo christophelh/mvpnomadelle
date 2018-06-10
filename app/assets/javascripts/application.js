0ab87f309117c0856e09fc920dc6811f1ffc7dfa
christophe <chris.l.honore@gmail.com>
2018-06-10 13:45:13 +0200

    search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,55 +15,10 @@
 //   });
 // });
 
-
-
-// Get user location
-
-// var x = document.querySelectorAll("locate");
-
-// function getLocation() {
-//     if (navigator.geolocation) {
-//         navigator.geolocation.watchPosition(showPosition);
-//     } else {
-//         x.innerHTML = "Geolocation is not supported by this browser.";}
-//     }
-
-// function showPosition(position) {
-//     x.innerHTML="Latitude: " + position.coords.latitude +
-//     "<br>Longitude: " + position.coords.longitude;
-// }
-
-
-      // function initMap() {
-      //   var map = new google.maps.Map(document.getElementById('map'), {
-      //     center: {lat: -34.397, lng: 150.644},
-      //     zoom: 6
-      //   });
-      //   var infoWindow = new google.maps.InfoWindow({map: map});
-
-      //   // Try HTML5 geolocation.
-      //   if (navigator.geolocation) {
-      //     navigator.geolocation.getCurrentPosition(function(position) {
-      //       var pos = {
-      //         lat: position.coords.latitude,
-      //         lng: position.coords.longitude
-      //       };
-
-      //       infoWindow.setPosition(pos);
-      //       infoWindow.setContent('Location found.');
-      //       map.setCenter(pos);
-      //     }, function() {
-      //       handleLocationError(true, infoWindow, map.getCenter());
-      //     });
-      //   } else {
-      //     // Browser doesn't support Geolocation
-      //     handleLocationError(false, infoWindow, map.getCenter());
-      //   }
-      // }
-
-      // function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-      //   infoWindow.setPosition(pos);
-      //   infoWindow.setContent(browserHasGeolocation ?
-      //                         'Error: The Geolocation service failed.' :
-      //                         'Error: Your browser doesn\'t support geolocation.');
-      // }
+<script>
+$(document).ready(function () {
+    if(window.location.href.indexOf("france") > -1) {
+       alert("your url contains the name franky");
+    }
+});
+</script>
