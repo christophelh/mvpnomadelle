95a951aaa0648044bef21320700e22ab5db3a07d
christophe <chris.l.honore@gmail.com>
2018-06-14 00:25:01 +0100

    search

--- db/migrate/20180613163956_addfieldontraception.rb
+++
@@ -1,9 +0,0 @@
-class Addfieldontraception < ActiveRecord::Migration[5.1]
-  def change
-    def change
-      add_column :contraceptionpills, :name, :string,
-      add_column :contraceptionpills, :country, :string,
-      add_column :contraceptionpills, :equivalent, :string
-    end
-  end
-end
