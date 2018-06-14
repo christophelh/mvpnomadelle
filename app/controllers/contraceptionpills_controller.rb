e5b71233b382cf750ad3655c06da7dc36b8beee5
christophe <chris.l.honore@gmail.com>
2018-06-14 13:04:50 +0100

    import fixed

--- app/controllers/contraceptionpills_controller.rb
+++ app/controllers/contraceptionpills_controller.rb
@@ -1,7 +1,7 @@
 class ContraceptionpillsController < ApplicationController
       def index
         if params[:query].present?
-          sql_query = "name ILIKE :query AND country ILIKE :country"
+          sql_query = "(name ILIKE :query) AND (country ILIKE :country)"
           @contraceptionpills = Contraceptionpill.where(sql_query, query: "%#{params[:query]}%", country: "%#{params[:country]}%")
         else
           @contraceptionpills = Contraceptionpill.all
