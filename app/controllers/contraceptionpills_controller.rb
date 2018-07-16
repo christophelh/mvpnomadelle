3c45670d8271ce73794d1f760c8bb18ae824a0cf
christophe <chris.l.honore@gmail.com>
2018-07-16 11:47:58 +0200

    search dropdown

--- app/controllers/contraceptionpills_controller.rb
+++ app/controllers/contraceptionpills_controller.rb
@@ -1,7 +1,7 @@
 class ContraceptionpillsController < ApplicationController
       def index
         if params[:query].present?
-          sql_query = "(country ILIKE :country) and (name ILIKE :query) OR (ingredient ILIKE :ingredient) AND (country ILIKE :country) OR (country ILIKE :country) and (name ILIKE :query) AND (ingredient ILIKE :ingredient)"
+          sql_query = "(ingredient ILIKE :ingredient) AND (country ILIKE :country) OR (country ILIKE :country) and (name ILIKE :query) AND (ingredient ILIKE :ingredient)"
           @contraceptionpills = Contraceptionpill.where(sql_query, query: "%#{params[:query]}%", country: "%#{params[:country]}%" , ingredient: "%#{params[:ingredient]}%")
         else
           @contraceptionpills = Contraceptionpill.all
